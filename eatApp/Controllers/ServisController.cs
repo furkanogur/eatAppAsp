c4aa4e18130ab63b25f0ec458ed69864d1e65a77
f2durann <20191132021@ogr.akdeniz.edu.tr>
2021-04-18 00:35:18 +0300

    SON

--- eatApp/Controllers/ServisController.cs
+++ eatApp/Controllers/ServisController.cs
@@ -11,34 +11,42 @@ namespace eatApp.Controllers
 {
     public class ServisController : ApiController
     {
-        YemekAppEntities1 db = new YemekAppEntities1();
+        eatAppEntities db = new eatAppEntities();
         sonucModel sonuc = new sonucModel();
 
         #region yemek
         [HttpGet]
         [Route("api/yemeklistele")]
-        public List<yemeklerModel> YemekListele()
+        public List<yemekModel> YemekListele()
         {
-            List<yemeklerModel> liste = db.Yemekler.Select(x => new yemeklerModel()
+            List<yemekModel> liste = db.Yemek_Tablosu.Select(x => new yemekModel()
             {
-                Id = x.Id,
-                YemekAdi = x.YemekAdi,
-                Tarif = x.Tarif,
+                yeme_Id = x.yeme_Id,
+                yemek_Adi = x.yemek_Adi,
+                yemek_Kategori_Id = x.yemek_Kategori_Id,
+                yemek_Malezeme_Id = x.yemek_Malezeme_Id,
+                yemek_Yapan_Id = x.yemek_Yapan_Id,
+                yemek_Tarif = x.yemek_Tarif
+
             }).ToList();
             return liste;
         }
 
         [HttpPost]
         [Route("api/yemekekle")]
-        public sonucModel YemekEkle(yemeklerModel kullanıcıdangelen)
+        public sonucModel YemekEkle(yemekModel model)
         {
 
-            Yemekler yeniyemek = new Yemekler();
-            yeniyemek.Id = kullanıcıdangelen.Id;
-            yeniyemek.YemekAdi = kullanıcıdangelen.YemekAdi;
-            yeniyemek.Tarif = kullanıcıdangelen.Tarif;
+            Yemek_Tablosu yeniyemek = new Yemek_Tablosu();
+            yeniyemek.yeme_Id = Guid.NewGuid().ToString();
+            yeniyemek.yemek_Adi = model.yemek_Adi;
+            yeniyemek.yemek_Kategori_Id = model.yemek_Kategori_Id;
+            yeniyemek.yemek_Malezeme_Id = model.yemek_Malezeme_Id;
+            yeniyemek.yemek_Tarif = model.yemek_Tarif;
+            yeniyemek.yemek_Yapan_Id = model.yemek_Yapan_Id;
 
-            db.Yemekler.Add(yeniyemek);
+
+            db.Yemek_Tablosu.Add(yeniyemek);
             db.SaveChanges();
             sonuc.islem = true;
             sonuc.mesaj = "Yeni yemek Eklendi";
@@ -46,5 +54,74 @@ namespace eatApp.Controllers
         }
 
         #endregion
+
+        [HttpPost]
+        [Route("api/uyeekle")]
+        public sonucModel UyeEkle(uyeModel model)
+        {
+            if (db.Uye_Tablosu.Count(s => s.uye_E_Mail == model.uye_E_Mail) > 0)
+            {
+                sonuc.islem = false;
+                sonuc.mesaj = "Kayıtlı kullanıcı tekrar kayıt edilemez!";
+                return sonuc;
+            }
+            Uye_Tablosu yeniUye = new Uye_Tablosu();
+            yeniUye.uye_Id = Guid.NewGuid().ToString();
+            yeniUye.uye_Adı_Soyado = model.uye_Adı_Soyado;
+            yeniUye.uye_E_Mail = model.uye_E_Mail;
+            yeniUye.uye_Sifre = model.uye_Sifre;
+            
+            db.Uye_Tablosu.Add(yeniUye);
+            db.SaveChanges();
+            sonuc.islem = true;
+            sonuc.mesaj = "Yeni Üye Eklendi";
+            return sonuc;
+        }
+
+        [HttpGet]
+        [Route("api/uyelistele")]
+        public List<uyeModel> UyeListele()
+        {
+            List<uyeModel> liste = db.Uye_Tablosu.Select(x => new uyeModel()
+            {
+                uye_Id = x.uye_Id,
+                uye_Adı_Soyado = x.uye_Adı_Soyado,
+                uye_E_Mail = x.uye_E_Mail,
+                uye_Sifre = x.uye_Sifre
+            }).ToList();
+            return liste;
+        }
+
+        [HttpGet]
+        [Route("api/uyebyid/{uyeId}")]
+        public uyeModel UyeById(string uyeId)
+        {
+            uyeModel kayit = db.Uye_Tablosu.Where(s => s.uye_Id == uyeId).Select(x => new uyeModel()
+            {
+                uye_Id = x.uye_Id,
+                uye_Adı_Soyado = x.uye_Adı_Soyado,
+                uye_E_Mail = x.uye_E_Mail,
+                uye_Sifre = x.uye_Sifre
+            }).SingleOrDefault();
+            return kayit;
+        }
+        [HttpDelete]
+        [Route("api/yemeksil/{yemekıd}")]
+        public sonucModel YemekSil(string yemekıd)
+        {
+
+            Yemek_Tablosu yemeksil = db.Yemek_Tablosu.Where(s => s.yeme_Id == yemekıd).SingleOrDefault();
+            if (yemeksil == null)
+            {
+                sonuc.islem = false;
+                sonuc.mesaj = "yemek bulunamadığı için silinemedi";
+                return sonuc;
+            }
+            db.Yemek_Tablosu.Remove(yemeksil);
+            db.SaveChanges();
+            sonuc.islem = true;
+            sonuc.mesaj = "yemek silindi";
+            return sonuc;
+        }
     }
 }
