fd650ddbffc9a8721a332e7d4fd257aef6337e40
f2durann <20191132021@ogr.akdeniz.edu.tr>
2021-04-17 22:24:07 +0300

    deneme2

--- eatApp/Controllers/ServisController.cs
+++ eatApp/Controllers/ServisController.cs
@@ -30,26 +30,18 @@ namespace eatApp.Controllers
 
         //[HttpPost]
         //[Route("api/yemekekle")]
-        //public sonucModel YemekEkle(urunBilgisiModel model)
+        //public sonucModel YemekEkle(yemeklerModel model)
         //{
 
-        //    Urun_Bilgisi yeniUrun = new Urun_Bilgisi();
-        //    yeniUrun.urun_Id = Guid.NewGuid().ToString();
-        //    yeniUrun.urun_Admin_Bilgi = model.urun_Admin_Bilgi;
-        //    yeniUrun.urun_Marka_Id = model.urun_Marka_Id;
-        //    yeniUrun.urun_Kategori_Id = model.urun_Kategori_Id;
-        //    yeniUrun.urun_Satis_Fiyat = model.urun_Satis_Fiyat;
-        //    yeniUrun.urun_KDV = model.urun_KDV;
-        //    yeniUrun.urun_Satılan = model.urun_Satılan;
-        //    yeniUrun.urun_Stok = model.urun_Stok;
-        //    yeniUrun.urun_İmage = model.urun_İmage;
-        //    yeniUrun.urun_Gelis_Fiyat = model.urun_Gelis_Fiyat;
-        //    yeniUrun.urun_Eklenme_Tarih = model.urun_Eklenme_Tarih;
-        //    yeniUrun.urun_Adi = model.urun_Adi;
-        //    db.Urun_Bilgisi.Add(yeniUrun);
+        //    Yemekler yeniyemek = new Yemekler();
+        //    //yeniyemek.Id = Guid.NewGuid().ToString();
+        //    yeniyemek.YemekAdi = model.YemekAdi;
+        //    yeniyemek.Tarif = model.Tarif;
+           
+        //    db.Yemekler.Add(yeniyemek);
         //    db.SaveChanges();
         //    sonuc.islem = true;
-        //    sonuc.mesaj = "Yeni Üye Eklendi";
+        //    sonuc.mesaj = "Yeni yemek Eklendi";
         //    return sonuc;
         //}
 
