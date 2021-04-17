b87db8587242157f199117b9dc1c8a008418a648
f2durann <20191132021@ogr.akdeniz.edu.tr>
2021-04-17 21:39:34 +0300

    deneme

--- eatApp/Controllers/ServisController.cs
+++ eatApp/Controllers/ServisController.cs
@@ -5,11 +5,13 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using eatApp.Models;
+using eatApp.ViewModel;
 
 namespace eatApp.Controllers
 {
     public class ServisController : ApiController
     {
         YemekAppEntities2 db = new YemekAppEntities2();
+        sonucModel sonuc = new sonucModel();
     }
 }
