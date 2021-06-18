678ec87b53f82bb0c8294f988c0f5a36b92f92d3
furkanogur <20191132035@ogr.akdeniz.edu.tr>
2021-06-18 15:05:08 +0300

    18.06.2021 //15:05

---
+++ eatApp/Auth/uyeService.cs
@@ -0,0 +1,29 @@
+﻿using eatApp.Models;
+using eatApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eatApp.Auth
+{
+    public class uyeService
+    {
+        YemekAppEntities db = new YemekAppEntities();
+        public uyeModel UyeOturumAc(string kadi, string parola)
+        {
+            uyeModel uye = db.uye_Tablosu.Where(s => s.uyeEmail == kadi && s.uyeSifre == parola).Select(x => new uyeModel()
+            {
+                uyeId = x.uyeId,
+                uyeAdSoyad = x.uyeAdSoyad,
+                uyeEmail = x.uyeEmail,
+                uyeSifre = x.uyeSifre,
+                uyeTelefon = x.uyeTelefon,
+                uyeAdmin = x.uyeAdmin,
+
+
+            }).SingleOrDefault();
+            return uye;
+        }
+    }
+}
